f0e412620cfa24dbe610abd5bf5815f1fd075d55
Rei Setogawa <rset6207@uni.sydney.edu.au>
2023-09-06 13:20:14 +1000

    Added side progress indicator and restructured project layout

---
+++ public/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    $(window).on('scroll', function() {
+        // Reset the styles on all icons in the side-nav
+        $('.side-nav span').css({
+            'background-color': 'transparent',
+            'color': 'black',
+            'transform': 'rotate(45deg)'
+        }).removeClass('active');
+
+        // Check which section is in view and style the corresponding icon in the side-nav
+        $('#section2,#section3,#section4, .landing').each(function(index) {
+            var top = $(this).offset().top;
+            var bottom = top + $(this).outerHeight();
+
+            if ($(window).scrollTop() >= top && $(window).scrollTop() <= bottom) {
+                var correspondingIcon = $('.side-nav span').eq(index);
+                correspondingIcon.css({
+                    'background-color': 'black',
+                    'color': 'white',
+                    'transform': 'rotate(45deg) scale(1.2)'
+                }).addClass('active');
+                return false; // Exit the loop
+            }
+        });
+    });
+});

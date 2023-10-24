e0074abc05d87013a6c96d54cb529db64ea42f06
Rei Setogawa <rset6207@uni.sydney.edu.au>
2023-10-24 20:08:51 +1100

    added sunburst chart in aboutme to visualise my skill set

--- public/script.js
+++ public/script.js
@@ -1,47 +1,182 @@
 import * as d3 from "d3";
 var Plotly = require('plotly.js-dist-min');
 
-d3.csv("/methods_data.csv", function(err, rows) {
-  if (err) {
-    console.error('Error reading CSV file:', err);
-    return;
-  }
+document.addEventListener("DOMContentLoaded", function () {
+  // Define the CHART and TESTER elements
+  const CHART = document.getElementById("methods_chart");
 
-  console.log('CSV data:', rows); // Debugging statement
+  var data = [{
 
-  try {
-    // Check if rows is an array before using map
-    if (Array.isArray(rows)) {
-      function unpack(rows, key) {
-        return rows.map(function (row) {
-          return row[key];
-        });
-      }
+    type: "sunburst",
+  
+    ids: [
+      "Discover",
+      "Define",
+      "Develop",
+      "Deliver",
+      "Background Research",
+      "Literature Review",
+      "Data Analysis",
+      "Case Studies",
+      "Market Analysis",
+      "Competitor Analysis",
+      "UX Research",
+      "Contextual Observation",
+      "A/B Testing",
+      "Heuristic Evaluation",
+      "Card Sorting",
+      "Usability Testing",
+      "Focus Groups",
+      "Online Ethnography",
+      "Interviews",
+      "Mind Mapping",
+      "Decision Matrix",
+      "Data Visualization",
+      "5 Whys",
+      "Statistical Analysis",
+      "Thematic Analysis",
+      "Affinity Diagramming",
+      "Wire Framing",
+      "Mock-ups",
+      "3D Rendering/Printing",
+      "Mood Boards",
+      "User Journey Mapping",
+      "Story Boarding",
+      "User Personas",
+      "Prototyping",
+      "Low-Fidelity Prototyping",
+      "High Fidelity Prototyping",
+      "Video Prototyping",
+      "Paper Prototyping",
+      "Sketching",
+      "Experience Prototyping",
+      "Persona-based Walkthroughs",
+      "Body Storming",
+      "Wizard of Oz Testing",
+      "Prototype Walkthroughs",
+      "Comparative Testing",
+      "Usability Testing"
+    ],
+  
+    labels: [
+      "Discover",
+      "Define",
+      "Develop",
+      "Deliver",
+      "Background Research",
+      "Literature Review",
+      "Data Analysis",
+      "Case Studies",
+      "Market Analysis",
+      "Competitor Analysis",
+      "UX Research",
+      "Contextual Observation",
+      "A/B Testing",
+      "Heuristic Evaluation",
+      "Card Sorting",
+      "Usability Testing",
+      "Focus Groups",
+      "Online Ethnography",
+      "Interviews",
+      "Mind Mapping",
+      "Decision Matrix",
+      "Data Visualization",
+      "5 Whys",
+      "Statistical Analysis",
+      "Thematic Analysis",
+      "Affinity Diagramming",
+      "Wire Framing",
+      "Mock-ups",
+      "3D Rendering/Printing",
+      "Mood Boards",
+      "User Journey Mapping",
+      "Storyboarding",
+      "User Personas",
+      "Prototyping",
+      "Low-Fidelity Prototyping",
+      "High Fidelity Prototyping",
+      "Video Prototyping",
+      "Paper Prototyping",
+      "Sketching",
+      "Experience Prototyping",
+      "Persona-based Walkthroughs",
+      "Body Storming",
+      "Wizard of Oz Testing",
+      "Prototype Walkthroughs",
+      "Comparative Testing",
+      "Usability Testing"
+    ],
+  
+    parents: [
+      "Methods",
+      "Methods",
+      "Methods",
+      "Methods",
+      "Discover",
+      "Background Research",
+      "Background Research",
+      "Background Research",
+      "Background Research",
+      "Background Research",
+      "Discover",
+      "UX Research",
+      "UX Research",
+      "UX Research",
+      "UX Research",
+      "UX Research",
+      "UX Research",
+      "UX Research",
+      "UX Research",
+      "Define",
+      "Define",
+      "Define",
+      "Define",
+      "Define",
+      "Define",
+      "Define",
+      "Develop",
+      "Develop",
+      "Develop",
+      "Develop",
+      "Develop",
+      "Develop",
+      "Develop",
+      "Develop",
+      "Prototyping",
+      "Prototyping",
+      "Prototyping",
+      "Low-Fidelity Prototyping",
+      "Low-Fidelity Prototyping",
+      "Deliver",
+      "Deliver",
+      "Deliver",
+      "Deliver",
+      "Deliver",
+      "Deliver",
+      "Deliver"
+  
+    ],
+  
+    outsidetextfont: {size: 20, color: "#377eb8"},
+  
+    // leaf: {opacity: 0.4},
+  
+    marker: {line: {width: 2}},
+  
+  }];
+  
+  
+  var layout = {
+  
+    margin: {l: 0, r: 0, b: 0, t:0},
+  
+    sunburstcolorway:["#636efa","#ef553b","#00cc96"],
+  
+  };
+  
+  
+    Plotly.newPlot(CHART, data, layout);
+  });
 
-      var data = [
-        {
-          type: "sunburst",
-          maxdepth: 3,
-          ids: unpack(rows, 'ids'),
-          labels: unpack(rows, 'labels'),
-          parents: unpack(rows, 'parents')
-        }
-      ];
+  // The code below is moved inside the d3.csv callback function or wrapped in an IIFE
 
-      var layout = {
-        margin: { l: 0, r: 0, b: 0, t: 0 },
-        sunburstcolorway: [
-          "#636efa", "#EF553B", "#00cc96", "#ab63fa", "#19d3f3",
-          "#e763fa", "#FECB52", "#FFA15A", "#FF6692", "#B6E880"
-        ],
-        extendsunburstcolorway: true
-      };
-
-      Plotly.newPlot('methods_chart', data, layout, { showSendToCloud: true });
-    } else {
-      console.error('CSV data is not an array.');
-    }
-  } catch (error) {
-    console.error('An error occurred:', error);
-  }
-});

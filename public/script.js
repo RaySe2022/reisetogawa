a01e3756c3d58367cacd0e4ebef3695f349cf191
Rei Setogawa <rset6207@uni.sydney.edu.au>
2023-11-15 18:43:52 +1100

    added sliding img animation on the gallery board

--- public/script.js
+++ public/script.js
@@ -1,27 +1,25 @@
-
-
-
-  //-----------------TYPE WRITER--------------------------------------
-
-  document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function() {
     typeWriter();
+    document.addEventListener("click", displayAllContent); // Add this line
 });
 
 let i = 0;
 let textIndex = 0;
+let typingInProgress = true; // Flag to check if typing is in progress
 const texts = [
     "Hi, I'm", 
-    "REI",
-    "SETOGAWA",
+    "REI SETOGAWA",
     "UX DESIGNER",
     "I am a passionate UX designer who finds joy in crafting remarkable digital experiences through the lens of product design. I immerse myself in the art of empathizing with people to create solutions that resonate at both a functional and emotional level."
 ];
 const normalSpeed = 100;
-const fastSpeed = 30; // Faster typing speed for the last <p> element
+const fastSpeed = 30;
 const pElements = document.querySelectorAll(".landing_left_textbox p");
 
 function typeWriter() {
-    const speed = (textIndex === texts.length - 1) ? fastSpeed : normalSpeed; // Use faster speed for the last text
+    if (!typingInProgress) return; // Stop typing if all content is displayed
+
+    const speed = (textIndex === texts.length - 1) ? fastSpeed : normalSpeed;
 
     if (i < texts[textIndex].length) {
         pElements[textIndex].innerHTML += texts[textIndex].charAt(i);
@@ -36,7 +34,70 @@ function typeWriter() {
     }
 }
 
-function showButton(){
+function showButton() {
     const landing_btn = document.getElementById("see_btn_landing");
-    landing_btn.style.display="flex";
+    landing_btn.style.display = "flex";
 }
+
+function displayAllContent() {
+    if (typingInProgress) {
+        typingInProgress = false; // Set the flag to false
+
+        // Immediately display all texts
+        pElements.forEach((p, index) => {
+            p.innerHTML = texts[index];
+        });
+
+        showButton(); // Show the button
+    }
+}
+
+//--------------------------------------------------------------------------------------
+document.addEventListener("DOMContentLoaded", function() {
+    // Select all project sections
+    const projectSections = document.querySelectorAll('.project');
+
+    // Loop through each project section and add event listeners
+    projectSections.forEach(function(section) {
+        section.addEventListener('mouseover', function() {
+            expandImageFrame(this);
+        });
+
+        section.addEventListener('mouseout', function() {
+            resetImageFrame(this);
+        });
+    });
+});
+
+function expandImageFrame(section) {
+    const imgFrame = section.querySelector('.img_frame');
+    const img = section.querySelector('.project_img');
+
+    // Expand .img_frame to cover the whole section
+    imgFrame.style.position = 'absolute';
+    imgFrame.style.top = '0';
+    imgFrame.style.left = '0';
+    imgFrame.style.width = '100%';
+    imgFrame.style.height = '100%';
+    imgFrame.style.zIndex = '10';
+    img.style.maxHeight = '50vh'; 
+    img.style.width = '100%'; 
+    img.style.height = '100%'; 
+
+
+}
+
+function resetImageFrame(section) {
+    const imgFrame = section.querySelector('.img_frame');
+    const img = section.querySelector('.project_img');
+
+    // Reset .img_frame to its original style
+    imgFrame.style.position = '';
+    imgFrame.style.top = '';
+    imgFrame.style.left = '';
+    imgFrame.style.height = '';
+    imgFrame.style.width = '47.5%'; // Adjust this based on your layout
+    imgFrame.style.zIndex = '';
+}
+
+

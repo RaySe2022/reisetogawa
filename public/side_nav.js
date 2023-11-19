a0d7448a78b2e1532713e5bc630c055722788c83
Rei Setogawa <rset6207@uni.sydney.edu.au>
2023-11-19 20:18:49 +1100

    finished coding side-nav and finialised layout issues in project page -> doing gallery layout and effects

--- public/side_nav.js
+++ public/side_nav.js
@@ -1,5 +1,5 @@
-var toc = document.querySelector( '.side_nav' );
-var tocPath = document.querySelector( '.toc-marker path' );
+var toc = document.querySelector('.side_nav');
+var tocPath = document.querySelector('.toc-marker path');
 var tocItems;
 
 // Factor of screen size that the element must cross
@@ -10,144 +10,148 @@ var TOP_MARGIN = 0.1,
 var pathLength;
 
 var lastPathStart,
-		lastPathEnd;
+    lastPathEnd;
 
 window.addEventListener('resize', function() {
     checkProcessVisibility();
-    drawPath(); // Existing function to draw the path
-    }, false);
-    
+    drawPath();
+}, false);
+
 window.addEventListener('scroll', function() {
     checkProcessVisibility();
-    sync(); // Existing function to synchronize TOC
-  }, false);
+    sync();
+}, false);
 
 drawPath();
 checkProcessVisibility(); // Initial call to set up the visibility
 
-
 function checkProcessVisibility() {
     var processSection = document.getElementById('process');
-    
+
     if (!processSection) {
-      return; // Process section not found
+        return; // Process section not found
     }
-    
+
     var processRect = processSection.getBoundingClientRect();
     var sideNav = document.querySelector('.side_nav');
-    
+
     // Hide .side_nav when the bottom of #process section is at or above the top of the viewport
     if (processRect.bottom <= 0) {
-      sideNav.style.display = 'block';
+        sideNav.style.display = 'block';
     } else {
-      sideNav.style.display = 'none';
+        sideNav.style.display = 'none';
     }
-  }
-
-  
+}
 
 function drawPath() {
-  
-  tocItems = [].slice.call( toc.querySelectorAll( 'li' ) );
+    // Select all the <a> tags directly preceding the .section-link
+    tocItems = [].slice.call(toc.querySelectorAll('a + .section-link'));
 
-  // Cache element references and measurements
-  tocItems = tocItems.map( function( item ) {
-    var anchor = item.querySelector( 'a' );
-    var target = document.getElementById( anchor.getAttribute( 'href' ).slice( 1 ) );
+    // Cache element references and measurements
+    tocItems = tocItems.map(function(sectionLink) {
+        var anchor = sectionLink.previousElementSibling;
+        var target = document.getElementById(anchor.getAttribute('href').slice(1));
 
-    return {
-      listItem: item,
-      anchor: anchor,
-      target: target
-    };
-  } );
+        return {
+            sectionLink: sectionLink,
+            anchor: anchor,
+            target: target
+        };
+    });
 
-  // Remove missing targets
-  tocItems = tocItems.filter( function( item ) {
-    return !!item.target;
-  } );
+    // Remove missing targets
+    tocItems = tocItems.filter(function(item) {
+        return !!item.target;
+    });
 
-  var path = [];
-  var pathIndent;
+    var path = [];
+    var pathIndent;
 
-  tocItems.forEach( function( item, i ) {
+    tocItems.forEach(function(item, i) {
+        var x = item.anchor.offsetLeft - 5,
+            y = item.anchor.offsetTop,
+            height = item.anchor.offsetHeight;
 
-    var x = item.anchor.offsetLeft - 5,
-        y = item.anchor.offsetTop,
-        height = item.anchor.offsetHeight;
+        if (i === 0) {
+            path.push('M', x, y, 'L', x, y + height);
+            item.pathStart = 0;
+        } else {
+            if (pathIndent !== x) path.push('L', pathIndent, y);
+            path.push('L', x, y);
+            tocPath.setAttribute('d', path.join(' '));
+            item.pathStart = tocPath.getTotalLength() || 0;
+            path.push('L', x, y + height);
+        }
+        pathIndent = x;
+        tocPath.setAttribute('d', path.join(' '));
+        item.pathEnd = tocPath.getTotalLength();
+    });
 
-    if( i === 0 ) {
-      path.push( 'M', x, y, 'L', x, y + height );
-      item.pathStart = 0;
-    }
-    else {
-      // Draw an additional line when there's a change in
-      // indent levels
-      if( pathIndent !== x ) path.push( 'L', pathIndent, y );
-
-      path.push( 'L', x, y );
-      
-      // Set the current path so that we can measure it
-      tocPath.setAttribute( 'd', path.join( ' ' ) );
-      item.pathStart = tocPath.getTotalLength() || 0;
-      
-      path.push( 'L', x, y + height );
-    }
-    
-    pathIndent = x;
-    
-    tocPath.setAttribute( 'd', path.join( ' ' ) );
-    item.pathEnd = tocPath.getTotalLength();
-
-  } );
-  
-  pathLength = tocPath.getTotalLength();
-  
-  sync();
-  
+    pathLength = tocPath.getTotalLength();
+    sync();
 }
 
 function sync() {
-  
   var windowHeight = window.innerHeight;
-  
+
+  document.querySelectorAll('.side_nav a').forEach(function(anchor) {
+    anchor.classList.remove('side_nav_active');
+});
+
+
+tocItems.forEach(function(item) {
+  var targetBounds = item.target.getBoundingClientRect();
+
+  // Check if this section is in the viewport
+  if (targetBounds.bottom > windowHeight * TOP_MARGIN && targetBounds.top < windowHeight * (1 - BOTTOM_MARGIN)) {
+      item.anchor.classList.add('side_nav_active'); // Add active class
+  return  }
+});
+
   var pathStart = pathLength,
       pathEnd = 0;
-  
-  var visibleItems = 0;
-  
-  tocItems.forEach( function( item ) {
 
-    var targetBounds = item.target.getBoundingClientRect();
-    
-    if( targetBounds.bottom > windowHeight * TOP_MARGIN && targetBounds.top < windowHeight * ( 1 - BOTTOM_MARGIN ) ) {
-      pathStart = Math.min( item.pathStart, pathStart );
-      pathEnd = Math.max( item.pathEnd, pathEnd );
-      
-      visibleItems += 1;
-      
-      item.listItem.classList.add( 'visible' );
-    }
-    else {
-      item.listItem.classList.remove( 'visible' );
-    }
-    
-  } );
-  
+  var visibleItems = 0;
+
+  tocItems.forEach(function(item) {
+      var targetBounds = item.target.getBoundingClientRect();
+
+      if (targetBounds.bottom > windowHeight * TOP_MARGIN && targetBounds.top < windowHeight * (1 - BOTTOM_MARGIN)) {
+          pathStart = Math.min(item.pathStart, pathStart);
+          pathEnd = Math.max(item.pathEnd, pathEnd);
+
+          visibleItems += 1;
+
+          item.sectionLink.classList.add('visible');
+          item.anchor.classList.add('side_nav_active'); // Add active class to the main anchor
+
+          // Add 'active' class to all child anchors
+          var childAnchors = item.sectionLink.querySelectorAll('a');
+          childAnchors.forEach(function(anchor) {
+              anchor.classList.add('side_nav_active');
+          });
+      } else {
+          item.sectionLink.classList.remove('visible');
+          item.anchor.classList.remove('side_nav_active'); // Remove active class from the main anchor
+
+          // Remove 'active' class from all child anchors
+          var childAnchors = item.sectionLink.querySelectorAll('a');
+          childAnchors.forEach(function(anchor) {
+              anchor.classList.remove('side_nav_active');
+          });
+      }
+  });
+
   // Specify the visible path or hide the path altogether
   // if there are no visible items
-  if( visibleItems > 0 && pathStart < pathEnd ) {
-    if( pathStart !== lastPathStart || pathEnd !== lastPathEnd ) {
-      tocPath.setAttribute( 'stroke-dashoffset', '1' );
-      tocPath.setAttribute( 'stroke-dasharray', '1, '+ pathStart +', '+ ( pathEnd - pathStart ) +', ' + pathLength );
-      tocPath.setAttribute( 'opacity', 1 );
-    }
+  if (visibleItems > 0 && pathStart < pathEnd) {
+      tocPath.setAttribute('stroke-dashoffset', '1');
+      tocPath.setAttribute('stroke-dasharray', '1, ' + pathStart + ', ' + (pathEnd - pathStart) + ', ' + pathLength);
+      tocPath.setAttribute('opacity', 1);
+  } else {
+      tocPath.setAttribute('opacity', 0);
   }
-  else {
-    tocPath.setAttribute( 'opacity', 0 );
-  }
-  
+
   lastPathStart = pathStart;
   lastPathEnd = pathEnd;
-
 }
